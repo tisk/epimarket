6e76ea1cb69280f182bb8551104aee872c495cdd
baltzi_j <jean-francois.baltzinger@epitech.eu>
2011-12-08 15:31:02 +0100

    mis a jour de la bdd
    Signed-off-by: baltzi_j <jean-francois.baltzinger@epitech.eu>

--- SQL/epimarket.sql
+++ SQL/epimarket.sql
@@ -1,20 +1,75 @@
 DROP DATABASE IF EXISTS `epimarket`;
 CREATE DATABASE `epimarket` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-DROP TABLE IF EXISTS `epimarket`.`Category`;
-CREATE TABLE `epimarket`.`Category` (
-  `CategoryId` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Category Identifier',
-  `name` varchar(30) NOT NULL,
-  PRIMARY KEY (`CategoryId`,`name`)
+DROP TABLE IF EXISTS `epimarket`.`customer`;
+CREATE TABLE `epimarket`.`customer` (
+  `id` int(32) unsigned NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(30) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `adress` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(30) NOT NULL,
+  `date` DATE NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-DROP TABLE IF EXISTS `epimarket`.`Product`;
-CREATE TABLE `epimarket`.`Product` (
-  `ProductId` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Product Identifier',
-  `CategoryId` int(10) unsigned NOT NULL COMMENT 'Category Identifier',
-  `price` int(10) unsigned NOT NULL COMMENT 'price of the product',
-  `description` varchar(30) NOT NULL COMMENT 'description of the product',
-  `name` varchar(30) NOT NULL COMMENT 'name of the product',
-  PRIMARY KEY (`ProductId`,`name`)
+DROP TABLE IF EXISTS `epimarket`.`reduction_customer`;
+CREATE TABLE `epimarket`.`reduction_customer` (
+  `id` int(32) unsigned NOT NULL AUTO_INCREMENT,
+  `customer_id` varchar(30) NOT NULL,
+  `reduction_id` int(32) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `epimarket`.`reduction`;
+CREATE TABLE `epimarket`.`reduction` (
+  `id` int(32) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `description` varchar(512) NOT NULL,
+  `type` int(32) unsigned NOT NULL,
+  `value` int(32) unsigned NOT NULL,
+  `target` int(32) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `epimarket`.`order`;
+CREATE TABLE `epimarket`.`order` (
+  `id` int(32) unsigned NOT NULL AUTO_INCREMENT,
+  `order_id` int(32) unsigned NOT NULL
+  `product_id` int(32) unsigned NOT NULL
+  `customer_id` int(32) unsigned NOT NULL
+  `quantity` int(32) unsigned NOT NULL
+  `date` DATE NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `epimarket`.`product`;
+CREATE TABLE `epimarket`.`product` (
+  `id` int(32) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL
+  `description` varchar(512) NOT NULL
+  `picture` varchar(255) NOT NULL
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `epimarket`.`category`;
+CREATE TABLE `epimarket`.`category` (
+  `id` int(32) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL
+  `description` varchar(512) NOT NULL
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `epimarket`.`stock`;
+CREATE TABLE `epimarket`.`stock` (
+  `id` int(32) unsigned NOT NULL AUTO_INCREMENT,
+  `product_id` int(32) unsigned NOT NULL
+  `quantity` int(32) unsigned NOT NULL
+  `buy_price` int(32) unsigned NOT NULL
+  `sell_price` int(32) unsigned NOT NULL
+  `next_buying` DATE NOT NULL
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
