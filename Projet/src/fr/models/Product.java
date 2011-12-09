f109f27a3de6997bc2121e2f4139ddc8c248e611
weyzohorth <weyzohorth@gmail.com>
2011-12-09 14:46:00 +0100

    Compression des g/setter

--- Projet/src/fr/models/Product.java
+++ Projet/src/fr/models/Product.java
@@ -7,15 +7,15 @@ import com.mysql.jdbc.ResultSet;
 public class Product extends MyDbUtils
 {
 
-	private String name;
-	private String description;;
-	private String picture;
-	private int categoryId;
-	private int quantity;
-	private int buy_price;
-	private int sell_price;
-	private int price;
-	private int pid;
+	private String	name;
+	private String	description;	;
+	private String	picture;
+	private int		categoryId;
+	private int		quantity;
+	private int		buy_price;
+	private int		sell_price;
+	private int		price;
+	private int		pid;
 	
 	public Product() 
 	{
@@ -89,49 +89,19 @@ public class Product extends MyDbUtils
 	}
 	
 	
-	public void setName(String name) 
-	{
-		this.name = name;
-	}
-
-	public void setCategory(int i) {
-		// TODO Auto-generated method stub
-		this.categoryId = i;
-	}
-
-	public void setPicture(String i) {
-		// TODO Auto-generated method stub
-		this.picture = i;
-	}
-
-	public void setDescription(String string) {
-		// TODO Auto-generated method stub
-		this.description = string;
-	}
-	public void setPrice(int price) {
-		this.price = price;
-	}
-	public int getPrice() {
-		return price;
-	}
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-	public int getQuantity() {
-		return quantity;
-	}
-	public void setBuy_price(int buy_price) {
-		this.buy_price = buy_price;
-	}
-	public int getBuy_price() {
-		return buy_price;
-	}
-	public void setSell_price(int sell_price) {
-		this.sell_price = sell_price;
-	}
-	public int getSell_price() {
-		return sell_price;
-	}
+	public void setName(String name)			{ this.name = name; }
+	public void setCategory(int i)				{ this.categoryId = i; }
+	public void setPicture(String i)			{ this.picture = i; }
+	public void setDescription(String string)	{ this.description = string; }
+	public void setPrice(int price)				{ this.price = price; }
+	public void setQuantity(int quantity)		{ this.quantity = quantity; }
+	public void setBuy_price(int buy_price)		{ this.buy_price = buy_price; }
+	public void setSell_price(int sell_price)	{ this.sell_price = sell_price; }
+	
+	public int getPrice()		{ return price; }
+	public int getQuantity()	{ return quantity; }
+	public int getBuy_price()	{ return buy_price; }
+	public int getSell_price()	{ return sell_price; }
 
 	public String getAttr(int type)		
 	{	
